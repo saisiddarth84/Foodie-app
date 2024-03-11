39c739705c1bbb6618386e6bec40d7f92a03c942
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-03-11 15:09:59 +0530

    Implemented React Router DOM to manage navigation within the application.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
-import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom"
+
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log('Header rendered')
+
+  // if no dependency array => useEffect is called on every render
+  // if dependency array is empty = [] => useEffect is called on initial render(just once)
+  // if dependency array is [btnNameReact] => useEffect is called everytime btnNameReact is updated
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // },[btnNameReact])
+  
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,11 +23,17 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
-          <li className="cart-container">
-            <img src={LOGO_URL} className="cart-image" />
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li className="cart">
+            Cart
           </li>
           <button
             className="login-btn"
