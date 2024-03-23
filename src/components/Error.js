b79670f03cc6d354623c4a0a1bad7c4386aea204
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-03-23 13:58:17 +0530

    Added Customized error line

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,10 +3,10 @@ import { useRouteError } from "react-router-dom"
 const Error = () => {
   const err = useRouteError();
   return (
-    <div>
+    <div className="flex text-red-500 flex-col justify-center items-center text-3xl font-semibold h-[100vh]">
       <h1>Opps!!</h1>
       <h2>Something went wrong!!!</h2>
-      <h3>{err.status}: {err.statusText}</h3>
+      <h3>{err.status}{err.statusText}</h3>
     </div>
   )
 }
